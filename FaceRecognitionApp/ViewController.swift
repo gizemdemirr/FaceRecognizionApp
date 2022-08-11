2e3f952076a43a523b413b764933f572affd9840
Gizem <gizeemdemirr1@gmail.com>
2022-08-11 13:07:13 -0700

    facerecognizerapp first commit

--- FaceRecognitionApp/ViewController.swift
+++ FaceRecognitionApp/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import LocalAuthentication //doğrulama için gerekli kütüphane
 
 class ViewController: UIViewController {
 
+  
+    
+    
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        let authContext = LAContext()
+        var error : NSError?
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "is it you ?") { success, error in
+                if success == true {
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toViewController", sender: nil)
+                    }
+                    
+                } else {
+                    DispatchQueue.main.async {
+                    self.myLabel.text = "error"
+                    }
+                }
+            }
+            
+        }
+    }
+    
 }
 
